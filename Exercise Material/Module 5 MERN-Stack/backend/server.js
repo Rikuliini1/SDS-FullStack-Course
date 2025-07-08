8a9772b19c912685ff4da94bd31ddb4d6f0a051e
Rikuliini1 <rento.fox@gmail.com>
2025-07-08 20:47:27 +0300

    Module 5 exercise material 1/4

---
+++ Exercise Material/Module 5 MERN-Stack/backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const dotenv = require('dotenv').config()
+const colors = require('colors')
+const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+const port = process.env.PORT || 5000
+
+async function initialize() {
+    await connectDB()
+
+    const app = express()
+
+    app.use(express.json())
+    app.use(express.urlencoded({ extended: false }))
+
+    app.use('/api/goals', require('./routes/goalRoutes'))
+    app.use(errorHandler)
+
+    app.listen(port, () => console.log(`Server running on port ${port}`.cyan.underline))
+}
+
+initialize()
+
+// EOF
