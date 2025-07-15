519c3cb8672a0b697319ece18f784dc08f9ba4f3
Rikuliini1 <rento.fox@gmail.com>
2025-07-15 14:03:09 +0300

    update

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ async function initialize() {
     if (process.env.NODE_ENV === 'production') {
         app.use(express.static(path.join(__dirname, '../frontend/dist')))
 
-        app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'dist', 'index.html')))
+        app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', '/dist', 'index.html')))
     } else {
         app.get('/', (req, res) => res.send('Please set to production'))
     }
